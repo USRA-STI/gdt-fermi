2fe250448938617d218f46fa44e37cd2ed6ee846
Bill Cleveland <120688282+BillCleveland-USRA@users.noreply.github.com>
2023-04-04 16:52:06 -0500

    added some details to setup.py

--- setup.py
+++ setup.py
@@ -1,49 +1,75 @@
+# CONTAINS TECHNICAL DATA/COMPUTER SOFTWARE DELIVERED TO THE U.S. GOVERNMENT WITH UNLIMITED RIGHTS
+#
+# Contract No.: CA 80MSFC17M0022
+# Contractor Name: Universities Space Research Association
+# Contractor Address: 7178 Columbia Gateway Drive, Columbia, MD 21046
+#
+# Copyright 2017-2022 by Universities Space Research Association (USRA). All rights reserved.
+#
+# Developed by: William Cleveland and Adam Goldstein
+#               Universities Space Research Association
+#               Science and Technology Institute
+#               https://sti.usra.edu
+#
+# Developed by: Daniel Kocevski
+#               National Aeronautics and Space Administration (NASA)
+#               Marshall Space Flight Center
+#               Astrophysics Branch (ST-12)
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License
+# is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied. See the License for the specific language governing permissions and limitations under the
+# License.
+#
 import shutil
 from setuptools import setup, find_namespace_packages
-from gdt.core import data_path
-from src.gdt.missions.fermi import __version__
+import gdt.core as core
 from tests import tests_path
 
 if __name__ == '__main__':
     setup(
-        name='gdt-fermi',
-        version=__version__,
-        packages=find_namespace_packages(where='src/', include=['gdt.missions.*']),
-        package_dir={'': 'src'},
-        url='github.com/USRA-STI/gdt',
-        license='Apache 2.0',
+        name="gdt-fermi",
+        version=core.__version__,
+        description="Gamma-ray Data Tools: Fermi Mission",
         author='Cleveland, Goldstein, Kocevski',
-        description='The Gamma-ray Data Tools',
+        url='https://github.com/USRA-STI/gdt-fermi',
+        packages=find_namespace_packages(where='src', include=["*"]),
+        classifiers=[
+            "Development Status :: 5 - Production/Stable",
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: Apache Software License",
+            "Operating System :: MacOS",
+            "Operating System :: POSIX :: Linux",
+            "Topic :: Scientific/Engineering :: Astronomy",
+            "Topic :: Software Development :: Libraries",
+        ],
+        license_files=['license.txt'],
+        keywords=['astronomy', 'gammaray', 'gamma-ray', 'usra'],
+        package_dir={"": "src"},
         package_data={
             'gdt.missions.fermi': ['data/McIlwainL_Coeffs.npy']
         },
         include_package_data=True,
         python_requires='>=3.8',
         install_requires=[
-            'gdt-core',
+            'gdt-core>=2.0.0',
             'pyproj>=1.9.6',
             'numpy>=1.17.3',
             'scipy>=1.1.0',
-            'matplotlib',
+            'matplotlib>=3.7.1',
             'astropy>=3.1',
             'healpy>=1.12.4',
+            'cartopy>=0.21.1',
         ],
-        extras_require={
-            'docs': [
-                'Sphinx',
-                'astropy_sphinx_theme',
-                'nbsphinx',
-                'ipython',
-                'sphinx_automodapi',
-                'notebook'
-            ],
-            'test': [
-                'pytest'
-            ]
-        }
     )
+
     # Copy test_files.url
     src = tests_path / 'test_files.urls'
-    dest = data_path / 'fermi-gbm' / 'test_files.urls'
+    dest = core.data_path / 'fermi-gbm' / 'test_files.urls'
     dest.parent.mkdir(parents=True, exist_ok=True)
     shutil.copyfile(src, dest)
