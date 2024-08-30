2f435ee7037c99472f15abf309ae65254f979245
Joshua Wood <joshua.randall.wood@gmail.com>
2024-08-30 17:16:32 -0500

    Added full SAA polygon history for the GBM mission (#45)
    * updating to new BaseFinder from latest gdt-core API
    
    * updated to latest api, minor typo fixes
    
    * update dol documentation to the latest API
    
    * update ftp to finder now that it's no longer an FtpFinder
    
    * give the option to skip FTP unit tests
    
    * need to convert ogrid output to a list because numpy>=2.0.0 now returns a tuple
    
    * bump scipy and matplotlib version numbers to the minimums allowed by gdt-core. We can't go below these because gdt-fermi depends on gdt-core
    
    * updating colorbar to latest matplotlib API which changes draw_all to _draw_all
    
    * caught two places where we forgot to set allclose to a 1 percent tolerance. This is needed because float32 support differs across machines
    
    * caught another instance of matplotlib API change
    
    * allow for backward & forwards compatibility regarding numpy newbyteorder
    
    * added unit test for plotting classes
    
    * cast detmask to int instead of bool to make behavior consistent between numpy v1 and v2
    
    * increment version number to denote changes that address new APIs
    
    * added saa polygon history from full mission
    
    * fixed some typos in saa.py and updated unit test to match
    
    * increment minor version number to denote change to SAA region definitions"
    
    * updating docs with latest polygons
    
    * fixed date typo in comment block
    
    ---------
    
    Co-authored-by: Josh Wood <joshua.r.wood@nasa.gov>

--- docs/missions/fermi/gbm/saa.rst
+++ docs/missions/fermi/gbm/saa.rst
@@ -2,6 +2,18 @@
 .. |GbmSaa| replace:: :class:`~gdt.missions.fermi.gbm.saa.GbmSaa`
 .. |GbmSaaPolygon1| replace:: :class:`~gdt.missions.fermi.gbm.saa.GbmSaaPolygon1`
 .. |GbmSaaPolygon2| replace:: :class:`~gdt.missions.fermi.gbm.saa.GbmSaaPolygon2`
+.. |GbmSaaPolygon3| replace:: :class:`~gdt.missions.fermi.gbm.saa.GbmSaaPolygon3`
+.. |GbmSaaPolygon4| replace:: :class:`~gdt.missions.fermi.gbm.saa.GbmSaaPolygon4`
+.. |GbmSaaPolygon5| replace:: :class:`~gdt.missions.fermi.gbm.saa.GbmSaaPolygon5`
+.. |GbmSaaPolygon6| replace:: :class:`~gdt.missions.fermi.gbm.saa.GbmSaaPolygon6`
+.. |GbmSaaPolygon7| replace:: :class:`~gdt.missions.fermi.gbm.saa.GbmSaaPolygon7`
+.. |GbmSaaPolygon8| replace:: :class:`~gdt.missions.fermi.gbm.saa.GbmSaaPolygon8`
+.. |GbmSaaPolygon9| replace:: :class:`~gdt.missions.fermi.gbm.saa.GbmSaaPolygon9`
+.. |GbmSaaPolygon10| replace:: :class:`~gdt.missions.fermi.gbm.saa.GbmSaaPolygon10`
+.. |GbmSaaPolygon11| replace:: :class:`~gdt.missions.fermi.gbm.saa.GbmSaaPolygon11`
+.. |GbmSaaPolygon12| replace:: :class:`~gdt.missions.fermi.gbm.saa.GbmSaaPolygon12`
+.. |GbmSaaPolygon13| replace:: :class:`~gdt.missions.fermi.gbm.saa.GbmSaaPolygon13`
+.. |GbmSaaPolygon14| replace:: :class:`~gdt.missions.fermi.gbm.saa.GbmSaaPolygon14`
 .. |GbmSaaCollection| replace:: :class:`~gdt.missions.fermi.gbm.saa.GbmSaaCollection`
 .. |FermiEarthPlot| replace:: :class:`~gdt.missions.fermi.plot.FermiEarthPlot`
 
@@ -13,18 +25,50 @@ particle flux in the Earth's Van Allen belts. Whenever Fermi is near
 this region, the detectors are turned off and not collecting data.
 
 The boundary of the SAA region is defined using a polygon in latitude
-and longitude. The definition of this boundary was changed once during
-GBM operations, resulting in two distinct periods:
+and longitude. Several different SAA polygon definitions have been
+used over the course of the mission:
 
-* |GbmSaaPolygon1| **(Launch - July 23, 2024):** Original SAA boundary
-  used during the first 16 years of operations. This definition
+* |GbmSaaPolygon1| **(Launch - June 9, 2010):** Original SAA boundary
+  used during the first 2 years of operations. This definition
   is conservative and includes a large portion of the southern
   Atlantic ocean.
-* |GbmSaaPolygon2| **(July 23, 2024 - present):** Updated SAA boundary
+* |GbmSaaPolygon2| **(June 9, 2010 - June 10, 2010):** One day
+  test of a smaller SAA region. This region proved to be too small
+  and resulted in a higher number of local particle triggers.
+* |GbmSaaPolygon3| **(June 10, 2010 - July 8, 2010):** Several week
+  test of a polygon that was smaller than GbmSaaPolygon1 but larger
+  than GbmSaaPolygon2.
+* |GbmSaaPolygon4| **(July 8, 2010 - July 20, 2010):** Several week
+  test of a polygon that was larger than GbmSaaPolygon3.
+* |GbmSaaPolygon5| **(July 20, 2010 - June 7, 2022):** SAA boundary
+  definition used during the majority of the mission. This defines
+  a smaller polygon than GbmSaaPolygon1 in order to increase observing
+  time. It was built using knowledge gained from the three prior test regions.
+* |GbmSaaPolygon6| **(June 7, 2022 - June 9, 2022):** Several day test of a 
+  smaller polygon.
+* |GbmSaaPolygon7| **(June 9, 2022 - August 3, 2022):** Reversion
+  to GbmSaaPolygon5 shape.
+* |GbmSaaPolygon8| **(August 3, 2022 - August 11, 2022):** Several day test of
+  a smaller polygon.
+* |GbmSaaPolygon9| **(August 11, 2022 - November 15, 2023):** Reversion
+  to GbmSaaPolygon5 shape.
+* |GbmSaaPolygon10| **(November 15, 2022 - November 17, 2023):** Several day test of
+  a smaller polygon.
+* |GbmSaaPolygon11| **(November 17, 2022 - July 23, 2024):** Reversion
+  to GbmSaaPolygon5 shape.
+* |GbmSaaPolygon12| **(July 23, 2024 - August 6, 2024):** Smaller SAA boundary
   implemented during the fourth gravitational wave observing
-  run (O4). This definition is designed to increase detector uptime
-  by reducing the area of the SAA boundary to more closely
-  follow the limits of the Van Allen belts.
+  run (O4). This definition matches GbmSaaPolygon10 in shape and is designed
+  to increase observing time by reducing the area of the SAA boundary to 
+  more closely follow the limits of the Van Allen belts.
+* |GbmSaaPolygon13| **(August 6, 2024 - August 8, 2024):** Minor modification
+  to GbmSaaPolygon12 to reduce local particle triggers occurring
+  on the western edge of the SAA boundary.
+* |GbmSaaPolygon14| **(August 8, 2024 - Present):** Minor modification
+  to GbmSaaPolygon13 to reduce local particle triggers occurring
+  on the eastern edge of the SAA boundary.
+
+.. image:: saa_figs/all_polygons.png
 
 We can retrieve the latitude and longitude values for a specific
 boundary by initializing the corresponding class. For example,
@@ -35,10 +79,10 @@ we can obtain the original SAA boundary by using the
     >>> saa = GbmSaaPolygon1()
     >>> saa.latitude
     array([-30.   , -19.867,  -9.733,   0.4  ,   2.   ,   2.   ,  -1.   ,
-            -6.155,  -8.88 , -14.22 , -18.404, -30.   , -30.   ])
+        -6.155,  -8.88 , -14.22 , -18.404, -30.   , -30.   ])
     >>> saa.longitude
     array([ 33.9  ,  12.398,  -9.103, -30.605, -38.4  , -45.   , -65.   ,
-           -84.   , -89.2  , -94.3  , -94.3  , -86.1  ,  33.9  ])
+       -84.   , -89.2  , -94.3  , -94.3  , -86.1  ,  33.9  ])
 
 We can also plot the SAA boundary using |FermiEarthPlot|:
 
@@ -83,7 +127,7 @@ Backwards Compatability
 The |GbmSaa| class provides a copy of the original SAA definition
 in order to support compatibility with code written before API version 2.1.1,
 which only defined the original boundary. It is exactly equivalent to
-working with |GbmSaaPolygon1|:
+working with |GbmSaaPolygon5|:
 
     >>> from gdt.missions.fermi.gbm.saa import GbmSaa
     >>> saa = GbmSaa()
--- src/gdt/missions/fermi/gbm/saa.py
+++ src/gdt/missions/fermi/gbm/saa.py
@@ -32,25 +32,154 @@ from ..time import Time
 from gdt.core.geomagnetic import SouthAtlanticAnomaly
 from gdt.core.data_primitives import Range
 
-__all__ = ['GbmSaaPolygon1', 'GbmSaaPolygon2', 'GbmSaa', 'GbmSaaCollection']
+__all__ = ['GbmSaaPolygon1', 'GbmSaaPolygon2', 'GbmSaaPolygon3',
+           'GbmSaaPolygon4', 'GbmSaaPolygon5', 'GbmSaaPolygon6',
+           'GbmSaaPolygon7', 'GbmSaaPolygon8', 'GbmSaaPolygon9',
+           'GbmSaaPolygon10', 'GbmSaaPolygon11', 'GbmSaaPolygon12',
+           'GbmSaaPolygon13', 'GbmSaaPolygon14', 'GbmSaa',
+           'GbmSaaCollection']
+
 
 class GbmSaaPolygon1(SouthAtlanticAnomaly):
     """The coordinates of the GBM SAA boundary in latitude and East longitude
-     used from launch (0 seconds) until 19:15 UTC on July 23, 2024.
+    used from launch (0 seconds) until 18:30:09 UTC on June 9, 2010. This
+    is the original SAA polygon.
     """
-    _latitude = [-30.000, -19.867, -9.733, 0.400, 2.000, 2.000, -1.000,
-                 -6.155, -8.880, -14.220, -18.404, -30.000, -30.000]
-    _longitude = [33.900, 12.398, -9.103, -30.605, -38.400, -45.000, -65.000,
-                  -84.000, -89.200, -94.300, -94.300, -86.100, 33.900]
+    _latitude = [-30.000, -22.600, 2.500, 5.200, 5.200, 4.600,
+                 0.700, -8.600, -9.900, -12.500, -21.700, -30.000, -30.000]
+    _longitude = [33.900,  24.500, -18.600, -25.700, -36.000, -42.000,
+                  -58.800, -93.100, -97.500, -98.500, -92.100, -86.100,  33.900]
 
     _time_range = Range(Time(0, format='fermi'),
-                        Time(743454905, format='fermi'))
+                        Time(297801011, format='fermi'))
 
 
 class GbmSaaPolygon2(SouthAtlanticAnomaly):
     """The coordinates of the GBM SAA boundary in latitude and East longitude
-    used from 19:15 UTC on July 23, 2024 until mission end which uses a
-    placeholder value of Jan 1, 2050 for now.
+    used from 18:30:09 UTC on June 9, 2010 until 17:41:58 UTC on June 10, 2010.
+
+    Note: This smaller region was proposed by Jerry Fishman to increase
+    observing time. It resulted in too many local particle triggers so it
+    was adjusted via GbmSaaPolygon3 and GbmSaaPolygon4.
+    """
+    _latitude = [-30.000, -21.670, -13.330, -5.000, -6.000, -7.000,
+                 -9.000, -11.000, -14.000, -17.000, -23.500, -30.000]
+    _longitude = [21.000, 0.000, -21.000, -42.000, -50.000, -58.000,
+                  -65.000, -72.000, -76.000, -80.000, -80.000, -80.000]
+
+    _time_range = Range(Time(297801011, format='fermi'),
+                        Time(297884520, format='fermi'))
+
+
+class GbmSaaPolygon3(SouthAtlanticAnomaly):
+    """The coordinates of the GBM SAA boundary in latitude and East longitude
+    used from 17:41:58 UTC on June 10, 2010 until 13:34:00 UTC on July 8, 2010.
+
+    Note: This is a slightly larger area version of GbmSaaPolygon2 to
+    remove local particle triggers occurring near SAA entry/exit.
+    """
+    _latitude = [-30.000, -14.500, -9.300, 1.000, 2.000, 3.000,
+                 -2.000, -7.000, -11.000, -15.000, -22.500, -30.000]
+    _longitude = [26.000, -2.000, -11.300, -30.000, -37.500, -45.000,
+                  -62.500, -80.000, -85.000, -90.000, -88.500, -87.000]
+
+    _time_range = Range(Time(297884520, format='fermi'),
+                        Time(300288842, format='fermi'))
+
+
+class GbmSaaPolygon4(SouthAtlanticAnomaly):
+    """The coordinates of the GBM SAA boundary in latitude and East longitude
+    used from 13:34:00 UTC on July 8, 2010 until 14:13:00 UTC on July 20, 2010.
+
+    Note: This is a slightly larger area version of GbmSaaPolygon3 to
+    remove local particle triggers occurring near SAA entry/exit.
+    """
+    _latitude = [-30.000, -2.000, -0.500, 0.500, 1.000, 1.000,
+                 0.000, -2.000, -7.500, -11.500, -16.500, -30.000]
+    _longitude = [40.000, -27.000, -35.000, -47.000, -60.000, -65.000,
+                  -75.000, -82.000, -90.000, -94.000, -97.000, -92.000]
+
+    _time_range = Range(Time(300288842, format='fermi'),
+                        Time(301327982, format='fermi'))
+
+
+class GbmSaaPolygon5(SouthAtlanticAnomaly):
+    """The coordinates of the GBM SAA boundary in latitude and East longitude
+    used from 14:13:00 UTC on July 20, 2010 until 14:20:00 UTC on June 7, 2022.
+
+    Note: Revised polygon proposed by Jerry Fishman, Michael Briggs, and
+    Vandiver Chaplin. This is smaller area than the original GbmSaaPolygon1
+    but avoids issues with local particle triggers seen in GbmSaaPolygon2-4.
+    """
+    _latitude = [-30.000, -19.867, -9.733, 0.400, 2.000, 2.000,
+                 -1.000, -6.155, -8.880, -14.220, -18.404, -30.000, -30.000]
+    _longitude = [33.900, 12.398, -9.103, -30.605, -38.400, -45.000,
+                  -65.000, -84.000, -89.200, -94.300, -94.300, -86.100, 33.900]
+
+    _time_range = Range(Time(301327982, format='fermi'),
+                        Time(676304405, format='fermi'))
+
+
+class GbmSaaPolygon6(SouthAtlanticAnomaly):
+    """The coordinates of the GBM SAA boundary in latitude and East longitude
+    used from 14:20:00 UTC on June 7, 2022 until 19:35:00 UTC on June 9, 2022.
+
+    Note: Several day test of a new, smaller area polygon proposed by
+    Michael Briggs.
+    """
+    _latitude = [-30.000, -19.867, -9.733, -3.400, -2.000, -2.000,
+                 -5.000, -10.155, -12.880, -16.220, -22.404, -30.000]
+    _longitude = [-5.000, -16.398, -28.103, -35.605, -38.400, -45.000,
+                  -65.000, -84.000, -89.200, -90.300, -90.300, -86.100]
+
+    _time_range = Range(Time(676304405, format='fermi'),
+                        Time(676496105, format='fermi'))
+
+
+class GbmSaaPolygon7(GbmSaaPolygon5):
+    """The coordinates of the GBM SAA boundary in latitude and East longitude
+    used from 19:35:00 UTC on June 9, 2022 until 18:10:45 UTC on August 3, 2022.
+
+    Note: Reversion to the same shape as GbmSaaPolygon5 following
+    the test of GbmSaaPolygon6.
+    """
+    _time_range = Range(Time(676496105, format='fermi'),
+                        Time(681243050, format='fermi'))
+
+
+class GbmSaaPolygon8(SouthAtlanticAnomaly):
+    """The coordinates of the GBM SAA boundary in latitude and East longitude
+    used from 18:10:45 UTC on August 3, 2022 until 17:59:26 UTC on August 11, 2022.
+
+    Note: Several day test of a new, smaller area polygon proposed by
+    Michael Briggs.
+    """
+    _latitude = [-30.000, -19.867, -9.733, -3.400, 0.000, 0.000,
+                 -3.000, -8.155, -10.880, -16.220, -22.404, -30.000, -30.000]
+    _longitude = [0.000, -11.398, -23.103, -30.605, -36.400, -43.000,
+                  -65.000, -84.000, -89.200, -90.300, -90.300, -86.100, 0.000]
+
+    _time_range = Range(Time(681243050, format='fermi'),
+                        Time(681933571, format='fermi'))
+
+
+class GbmSaaPolygon9(GbmSaaPolygon5):
+    """The coordinates of the GBM SAA boundary in latitude and East longitude
+    used from 17:59:26 UTC on August 11, 2022 until 16:50:00 UTC on November 15, 2023.
+
+    Note: Reversion to the same shape as GbmSaaPolygon5 following
+    the test of GbmSaaPolygon8.
+    """
+    _time_range = Range(Time(681933571, format='fermi'),
+                        Time(721759805, format='fermi'))
+
+
+class GbmSaaPolygon10(SouthAtlanticAnomaly):
+    """The coordinates of the GBM SAA boundary in latitude and East longitude
+    used from 16:50:00 UTC on November 15, 2023 until 18:27:00 UTC on November 17, 2023.
+
+    Note: Several day test of a new, smaller area polygon proposed by
+    Michael Briggs.
     """
     _latitude = [-24.395, -30.000, -30.000, -30.000, -30.000, -24.060,
                  -16.220, -8.638, -6.155, -1.000, 2.000, 2.000,
@@ -59,13 +188,72 @@ class GbmSaaPolygon2(SouthAtlanticAnomaly):
                   -90.300, -88.738, -84.000, -65.000, -45.000, -38.400,
                   -30.605, -11.999457706441582, 22.000]
 
+    _time_range = Range(Time(721759805, format='fermi'),
+                        Time(721938425, format='fermi'))
+
+
+class GbmSaaPolygon11(GbmSaaPolygon5):
+    """The coordinates of the GBM SAA boundary in latitude and East longitude
+    used from 18:27:00 UTC on November 17, 2023 until 19:15:00 UTC on July 23, 2024.
+
+    Note: Reversion to the same shape as GbmSaaPolygon5 following
+    the test of GbmSaaPolygon10.
+    """
+    _time_range = Range(Time(721938425, format='fermi'),
+                        Time(743454905, format='fermi'))
+
+
+class GbmSaaPolygon12(GbmSaaPolygon10):
+    """The coordinates of the GBM SAA boundary in latitude and East longitude
+    used from 19:15 UTC on July 23, 2024 until 19:15:00 UTC on August 6, 2024.
+
+    Note: Initial implementation of the new, smaller area polygon proposed by
+    Michael Briggs.
+    """
     _time_range = Range(Time(743454905, format='fermi'),
+                        Time(744664505, format='fermi'))
+
+
+class GbmSaaPolygon13(SouthAtlanticAnomaly):
+    """The coordinates of the GBM SAA boundary in latitude and East longitude
+    used from 19:15:00 UTC on August 6, 2024 until 00:00:00 UTC on August 8, 2024.
+
+    Note: Minor adjustment to GbmSaaPolygon12 which reduces the number of local
+    particle triggers occurring during SAA entry (western side of polygon).
+    """
+    _latitude = [-24.395, -30.000, -30.000, -30.000, -30.000, -30.000,
+                 -30.000, -22.646, -16.220, -9.404, -6.155, -1.000,
+                 2.000, 2.000, -3.650, -20.136067618721196, -24.395]
+    _longitude = [22.000, 22.000, 5.500, 0.000, -2.000, -40.000,
+                  -86.100, -91.300, -91.800, -89.700, -84.000, -65.000,
+                  -45.000, -38.400, -30.605, -8.015646247668737, 22.000]
+
+    _time_range = Range(Time(744664505, format='fermi'),
+                        Time(744768005, format='fermi'))
+
+
+class GbmSaaPolygon14(SouthAtlanticAnomaly):
+    """The coordinates of the GBM SAA boundary in latitude and East longitude
+    used from 00:00:00 UTC on August 8, 2024 until mission end which uses a
+    placeholder value of Jan 1, 2050 for now.
+
+    Note: Minor adjustment to GbmSaaPolygon13 which reduces the number of local
+    particle triggers occurring during SAA exit (eastern side of polygon).
+    """
+    _latitude = [-24.395, -30.000, -30.000, -30.000, -30.000, -30.000,
+                 -30.000, -22.646, -16.220, -9.404, -6.155, -1.000,
+                 2.000, 2.000, -3.650, -17.867934687794072, -24.395]
+    _longitude = [22.000, 22.000, 5.500, 0.000, -2.340, -40.000,
+                  -86.100, -91.300, -91.800, -89.700, -84.000, -65.000,
+                  -45.000, -38.400, -30.605, -11.123461787369832, 22.000]
+
+    _time_range = Range(Time(744768005, format='fermi'),
                         Time(1546300805, format='fermi'))
 
 
-class GbmSaa(GbmSaaPolygon1):
+class GbmSaa(GbmSaaPolygon5):
     """Class providing backwards compatibility for code written prior to v2.1.1
-    where the GbmSaa class defined GbmSaaPolygon1"""
+    where the GbmSaa class defined GbmSaaPolygon5"""
     pass
 
 
@@ -74,7 +262,12 @@ class GbmSaaCollection():
 
     def __init__(self):
         """Constructor"""
-        self._polygons = [GbmSaaPolygon1(), GbmSaaPolygon2()]
+        self._polygons = [
+            GbmSaaPolygon1(), GbmSaaPolygon2(), GbmSaaPolygon3(),
+            GbmSaaPolygon4(), GbmSaaPolygon5(), GbmSaaPolygon6(),
+            GbmSaaPolygon7(), GbmSaaPolygon8(), GbmSaaPolygon9(),
+            GbmSaaPolygon10(), GbmSaaPolygon11(), GbmSaaPolygon12(),
+            GbmSaaPolygon13(), GbmSaaPolygon14()]
         self.sort()
 
     def at(self, time: Time):
